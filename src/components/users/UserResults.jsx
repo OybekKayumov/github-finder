8a91b736274a26de20d72bd46422cfb9f6e2a97e
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-05-15 14:14:34 +0500

    add UserResults.jsx: add condition in render data

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -22,14 +22,19 @@ function UserResults() {
     setUsers(data);
     setLoading(false);
   }
-
-  return (
-    <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
-      {users.map((user) => (
-        <h3>{user.login}</h3>
-      ))}
-    </div>
-  )
+  // if not loading
+  if (!loading) {
+    return (
+      <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
+        {users.map((user) => (
+          <h3>{user.login}</h3>
+        ))}
+      </div>
+    )
+  } else {
+    return <h3>Loading...</h3>
+  }
+  
 }
 
 export default UserResults
