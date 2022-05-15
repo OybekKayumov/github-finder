1835f2aa2b6aade175bc2bfa997c807b21193e76
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-05-15 08:49:54 +0500

    update Navbar

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import {FaGithub} from 'react-icons/fa'
+import {Link} from 'react-router-dom'
+import PropTypes from 'prop-types'
 
-function Navbar() {
+function Navbar({ title }) {
   return (
     <div>Navbar</div>
   )
+}
+
+Navbar.defaultProps = {
+  title: 'Github Finder'
+}
+
+Navbar.propTypes = {
+  title: PropTypes.string,
 }
 
 export default Navbar
