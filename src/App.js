89bec90f43bf51dab10ca7ca6dfc56be9d66927a
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-05-15 08:12:13 +0500

    add daisyui class btn into app.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-purple-500">
       <h1 className="text-3xl font-bold underline">
         Github Finder App
       </h1>
+      <button className='btn'>Daisy Class Button</button>
     </div>
   );
 }
