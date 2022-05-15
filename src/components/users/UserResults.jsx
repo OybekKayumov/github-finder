dda354e1689c430fb6b94abce53b3f960ad58099
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-05-15 13:57:00 +0500

    add components-user UserResults.jsx: fetch data from API-(30)

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,6 +1,22 @@
 import React from 'react'
+import { useEffect } from 'react'
 
 function UserResults() {
+  useEffect(() => {
+    fetchUsers();
+  }, [])
+
+  const fetchUsers = async () => {
+    const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
+      headers: {
+        Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`
+      }
+    })
+
+    const data = await response.json();
+    console.log('data: ', data);
+  }
+
   return (
     <div>UserResults</div>
   )
