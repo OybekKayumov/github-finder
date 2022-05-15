bfbd2869984fe87c04c18097b92c72b28c4ec08c
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-05-15 14:10:40 +0500

    add UserResults.jsx: get user login

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -25,7 +25,9 @@ function UserResults() {
 
   return (
     <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
-      UserResults
+      {users.map((user) => (
+        <h3>{user.login}</h3>
+      ))}
     </div>
   )
 }
