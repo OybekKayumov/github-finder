081241308f3d136128f647bfb0ab356ce7f1742f
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-05-16 14:09:39 +0500

    add userItem

---
+++ src/components/users/UserItem.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function UserItem() {
+  return (
+    <div>UserItem</div>
+  )
+}
+
+export default UserItem
--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useEffect, useState } from 'react'
 import Spinner from '../layout/Spinner'
+import UserItem from './UserItem'
 
 function UserResults() {
   const [users, setUsers] = useState([])
@@ -27,8 +28,8 @@ function UserResults() {
   if (!loading) {
     return (
       <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
-        {users.map((user) => (
-          <h3>{user.login}</h3>
+        {users.map((user) => (          
+          <UserItem key={user.id} user={user}/>
         ))}
       </div>
     )
@@ -46,3 +47,5 @@ export default UserResults
 
 
 // return <h3>Loading...</h3>
+
+/* <h3>{user.login}</h3> */
