20e09ad29b732133adb75ece0ed74d93a0eff35e
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-05-16 17:44:31 +0500

    add UserSearch: form input, btn, btn

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -2,7 +2,33 @@ import React from 'react'
 
 function UserSearch() {
   return (
-    <div>UserSearch</div>
+    <div className='grid grid-cols-1 xl:grid-cols-2 lg:grid-cols-2 mb:grid-cols-2 mb-8 gap-8'>
+      <div>
+        <form>
+          <div className='form-control'>
+            <div className='relative'>
+              <input 
+                type="text"
+                className='w-full pr-40 bg-orange-200 input input-lg text-black'
+                placeholder='Search User'
+              />
+              <button 
+                type="submit"
+                className='absolute top-0 right-0 rounded-l-none w-36 btn btn-lg'
+              >
+                Go
+              </button>
+            </div>
+
+          </div>
+        </form>
+
+      </div>
+
+      <div>
+        <button className='btn btn-ghost btn-lg'>Clear</button>
+      </div>
+    </div>
   )
 }
 
