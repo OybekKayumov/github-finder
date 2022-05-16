86c78c628d062b5a58a3b18fd32dbba7d0d7fdce
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-05-16 14:20:38 +0500

    add userItem user-login, avatar style

--- src/components/users/UserItem.jsx
+++ src/components/users/UserItem.jsx
@@ -3,7 +3,23 @@ import PropTypes from 'prop-types'
 
 function UserItem({user: { login, avatar_url }}) {
   return (
-    <div>{login}</div>
+    <div className='card shadow-md compact side bg-base-100'>
+      <div className='flex-row items-center space-x-4 card-body'>
+        <div>
+          <div className='avatar'>
+            <div className='rounded-full shadow w-14 h-14'>
+              <img src={avatar_url} alt="profile" />
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <h2 className='card-title'>{login}</h2>
+        </div>
+
+      </div>
+      
+    </div>
   )
 }
 
