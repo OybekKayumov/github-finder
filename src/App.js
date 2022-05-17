3bd674105edc97f79562d3e460ee746169ed5bb8
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-05-17 19:07:42 +0500

    add App,js wrap AlertProvider

--- src/App.js
+++ src/App.js
@@ -5,30 +5,33 @@ import Home from "./components/pages/Home.jsx";
 import About from "./components/pages/About.jsx";
 import NotFound from "./components/pages/NotFound.jsx";
 import { GithubProvider } from "./context/github/GithubContext.js";
+import { AlertProvider } from "./context/alert/AlertContext.js";
 
 function App() {
   return (
     <GithubProvider>
-      <Router className="App bg-purple-500">
-        <div className='flex flex-col justify-between h-screen'>
-          <Navbar />
+      <AlertProvider>
+        <Router className="App bg-purple-500">
+          <div className='flex flex-col justify-between h-screen'>
+            <Navbar />
 
-          <main className="container mx-auto px-3 pb-12">
-            <Routes>
+            <main className="container mx-auto px-3 pb-12">
+              <Routes>
 
-              <Route path="/" element={<Home />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/notfound" element={<NotFound />} />
-              <Route path="/*" element={<NotFound />} />
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/notfound" element={<NotFound />} />
+                <Route path="/*" element={<NotFound />} />
 
-            </Routes>
-            
-          </main>
+              </Routes>
+              
+            </main>
 
-          <Footer />
-        </div>  
+            <Footer />
+          </div>  
 
-      </Router>
+        </Router>
+      </AlertProvider>
     </GithubProvider>
   );
 }
