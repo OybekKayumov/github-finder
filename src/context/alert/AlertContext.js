adde37024d257b2f38abecb05b293ea50ff46c3c
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-05-17 19:12:05 +0500

    add setAlert 2000ms

--- src/context/alert/AlertContext.js
+++ src/context/alert/AlertContext.js
@@ -8,6 +8,16 @@ export const AlertProvider = ({children}) => {
 
   const [state, dispatch] = useReducer(alertReducer, initialState)
 
+  // set an alert
+  const setAlert = (msg, type) => {
+    dispatch({
+      type: 'SET_ALERT',
+      payload: {msg, type}
+    })
+
+    setTimeout(() => dispatch({ type: 'REMOVE_ALERT' }), 2000)
+  }
+
   return (
     <AlertContext.Provider value={{alert: state}}>
       {children}
