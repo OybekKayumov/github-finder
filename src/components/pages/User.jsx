4911c213ecf8474afbc11b9155952009fbd0ef8b
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-05-18 06:28:35 +0500

    add user.jsx: avatar_url - photo

--- src/components/pages/User.jsx
+++ src/components/pages/User.jsx
@@ -17,8 +17,36 @@ function User() {
     // getUserRepos(params.login)
   }, [])
 
+  const {
+    name, type, avatar_url, location, bio, twitter_username, login, html_url, followers, public_repos, public_gists, hireable
+  } = user
+
+  if (loading) {
+    return <Spinner />
+  }
+
   return (
-    <div>{user.login}</div>
+    <div className='w-full mx-auto lg:w-10/12'>
+      <div className='mb-4'>
+        <Link to='/' className='btn btn-ghost'>
+          Back To Search
+        </Link>
+      </div>
+
+      <div className='grid grid-cols-1 xl:grid-cols-3 lg:grid-cols-3 mb:grid-cols-3 mb-8 md:gap-8'>
+        <div className='custom-card-image mb-6 md:mb-0'>
+          <div className='rounded-lg shadow-xl card image-full'>
+            <figure>
+              <img src={avatar_url} alt="login image" />
+            </figure>
+
+          </div>
+
+        </div>
+
+      </div>
+      {user.login}
+    </div>
   )
 }
 
