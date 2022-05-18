7019f078adeb1e94629e8734c57d364158b8967b
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-05-18 20:12:55 +0500

    add repo/repoItem how to add fa-icon to badge

--- src/components/repos/RepoItem.jsx
+++ src/components/repos/RepoItem.jsx
@@ -1,9 +1,43 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import { FaEye, FaInfo, FaLink, FaStar, FaUtensils} from 'react-icons/fa'
 
-function RepoItem() {
+
+function RepoItem({ repo }) {
+
+  const {
+    name,
+    description,
+    html_url,
+    forks,
+    open_issues,
+    watchers_count,
+    stargazers_count,
+  } = repo;
+
   return (
-    <div>RepoItem</div>
+    <div className='mb-2 rounded-md card bg-orange-400 hover:bg-gray-500'>
+      <div className='card-body'>
+        <h3 className='mb-2 text-xl font-semibold'>
+          <a href={html_url}>
+            <FaLink className='inline mr-1' /> {name}
+          </a>
+        </h3>
+        <p className='mb-3'>{description}</p>
+        <div>
+          <div className='mr-2 badge badge-info badge-lg'>
+            <FaEye />
+
+          </div>
+        </div>
+      </div>  
+      
+    </div>
   )
+}
+
+RepoItem.propTypes = {
+  repo: PropTypes.object.isRequired,
 }
 
 export default RepoItem
