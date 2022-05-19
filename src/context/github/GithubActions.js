cec606d203c853875a5631e609b95e1de12fb9df
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-05-19 11:57:18 +0500

    add action getUser and getUserRepos into githubActions.js

--- src/context/github/GithubActions.js
+++ src/context/github/GithubActions.js
@@ -1,33 +1,70 @@
+// search users is stored in an action file
+// our component - when calling action then dispatch the action to the reducer
+// passing payload and update user's state 
+
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
 const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
 
-// get search result users (testing purposes) 2
-  // const fetchUsers = async () => {
+// get search result users
 export const searchUsers = async (text) => {
         
   const params = new URLSearchParams({
     q: text
   })
-
-  // const response = await fetch(`${GITHUB_URL}/users`, {
+  
   const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
     headers: {
       Authorization: `token ${GITHUB_TOKEN}`
     }
   })
 
-  // const data = await response.json();
   const {items} = await response.json();
-  // console.log('data: ', data);
 
-  // from useState
-  // setUsers(data);
-  // setLoading(false);
-
-  // dispatch({
-  //   type: 'GET_USERS',
-  //   // payload: data,
-  //   payload: items,
-  // })
   return items;
 }
+
+// get single user
+export const getUser = async (login) => {
+  setLoading()
+
+  const response = await fetch(`${GITHUB_URL}/users/${login}`, {
+    headers: {
+      Authorization: `token ${GITHUB_TOKEN}`
+    }
+  })
+
+  if (response.status === 404) {
+    window.location = '/notfound'
+  } else {    
+    const data = await response.json();
+    
+    dispatch({
+      type: 'GET_USER',    
+      payload: data,
+    })
+  }
+}
+
+// get user repos  
+export const getUserRepos = async (login) => {
+  setLoading()
+
+  const params = new URLSearchParams({
+    sort: 'created',
+    per_page: 10,
+  })
+    
+  const response = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`, {
+    headers: {
+      Authorization: `token ${GITHUB_TOKEN}`
+    }
+  })
+
+  const data = await response.json();
+
+  dispatch({
+    type: 'GET_REPOS',
+    payload: data,
+  })
+}
+
