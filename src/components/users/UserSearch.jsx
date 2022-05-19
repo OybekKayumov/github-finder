141137a130cb0a1a2e504e7744e6d072beb0aed0
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-05-19 11:41:20 +0500

    import userSearch in userSearch.js

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -2,8 +2,7 @@ import React from 'react'
 import { useState, useContext } from 'react'
 import GithubContext from '../../context/github/GithubContext'
 import AlertContext from '../../context/alert/AlertContext.js'
-
-
+import { searchUsers } from '../../context/github/GithubActions'
 
 function UserSearch() {
   const [text, setText] = useState('')
